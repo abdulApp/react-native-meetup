cc2f36cdb3ee05bfa3d25766c2b3f985b764a0a0
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-09-25 23:54:15 +0300

    put the outline

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,18 @@
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function Events() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
+      <Text>This is a test</Text>
+      <Text>This is a Date</Text>
+      <Text>This is a Title</Text>
+      <Text>This is a City Name</Text>
+
+      {/* Events Image */}
+
+
     </>
   );
 }
