b35a9bef5404f8d087f23040bc55fa1d77915d3c
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-09-26 11:29:34 +0300

    still work on the outline but add some style

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,20 +7,20 @@ export default function Events() {
     <>
       <Stack.Screen options={{ title: 'Events' }} />
       <Text>This is a test</Text>
-      <Text>This is a Date</Text>
-      <Text>This is a Title</Text>
+      <Text>Wed 13, Sep · 19:30 CET</Text>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>This is a Title</Text>
       <Text>This is a City Name</Text>
 
       {/* Events Image */}
       <Image
         source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg' }}
-        style={{ width: '90%', height: 200 }}
+        style={{ width: 200, height: 200 }}
       />
 
       {/* Footer */}
       <Text>16 going</Text>
-      <Feather name="share" size={24} color="black" />
-      <Feather name="bookmark" size={24} color="black" />
+      <Feather name="share" size={20} color="gray" />
+      <Feather name="bookmark" size={24} color="gray" />
     </>
   );
 }
