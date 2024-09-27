8bfea34afb8e26ec8bf3327fb9c30fb20c2d97ce
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-09-27 14:38:14 +0300

    Event details page

---
+++ app/[id].tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+import events from "~/assets/events.json"
+
+export default function EventPage() {
+  const { id } = useLocalSearchParams();
+
+  const event = events.find((event) => event.id === id);
+  return <Text>Event id: { event?.title }</Text>
+}
--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -2,12 +2,14 @@ import Feather from '@expo/vector-icons/Feather';
 import dayjs from 'dayjs';
 // import { Link } from 'expo-router';
 // import { useEffect, useState } from 'react';
+import { Link } from 'expo-router';
 import { View, Text, Image, Pressable } from 'react-native';
 
 // import SupaImage from './SupaImage';
 
 // import { supabase } from '~/utils/supabase';
 
+// @ts-ignore
 export default function EventListItem({ event }) {
   // const [numberOfAttendees, setNumberOfAttendees] = useState(0);
 
@@ -16,42 +18,41 @@ export default function EventListItem({ event }) {
   // }, [event.id]);
 
   // const fetchNumberOfAttendees = async () => {
-    // const { count } = await supabase
-    //   .from('attendance')
-    //   .select('*', { count: 'exact', head: true })
-    //   .eq('event_id', event.id);
-    //
-    // setNumberOfAttendees(count);
+  // const { count } = await supabase
+  //   .from('attendance')
+  //   .select('*', { count: 'exact', head: true })
+  //   .eq('event_id', event.id);
+  //
+  // setNumberOfAttendees(count);
   // };
 
   return (
     <>
-      <View className="gap-3 p-3">
-        <View className="flex-row">
-          <View className="flex-1 gap-2">
-            <Text className="text-lg font-semibold uppercase text-amber-800">
-              {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
-              {/*{event.datetime} · 19:30 CET*/}
-            </Text>
-            <Text className="text-xl font-bold">{event.title}</Text>
-            <Text className="text-gray-700">{event.location}</Text>
+      <Link href={`/${event.id}`} asChild>
+        <Pressable className="m-3 gap-3 border-b-2 border-gray-100 pb-3">
+          <View className="flex-row">
+            <View className="flex-1 gap-2">
+              <Text className="text-lg font-semibold uppercase text-amber-800">
+                {dayjs(event.datetime).format('ddd, D MMM')} ·{' '}
+                {dayjs(event.datetime).format('h:mm A')}
+                {/*{event.datetime} · 19:30 CET*/}
+              </Text>
+              <Text className="text-xl font-bold">{event.title}</Text>
+              <Text className="text-gray-700">{event.location}</Text>
+            </View>
+
+            {/* Events Image */}
+            <Image source={{ uri: event.image }} className="aspect-video w-2/5 rounded-xl" />
           </View>
 
-          {/* Events Image */}
-          <Image
-            source={{ uri: event.image }}
-            className="aspect-video w-2/5 rounded-xl"
-          />
-        </View>
-
-        {/* Footer */}
-        <View className="flex-row gap-3">
-
-          <Text className="mr-auto text-gray-700">16 going</Text>
-          <Feather name="share" size={20} color="gray" />
-          <Feather name="bookmark" size={24} color="gray" />
-        </View>
-      </View>
+          {/* Footer */}
+          <View className="flex-row gap-3">
+            <Text className="mr-auto text-gray-700">16 going</Text>
+            <Feather name="share" size={20} color="gray" />
+            <Feather name="bookmark" size={24} color="gray" />
+          </View>
+        </Pressable>
+      </Link>
       {/*<Link href={`/event/${event.id}`} asChild>*/}
       {/*  <Pressable className="m-3 gap-3 border-b-2 border-gray-100 pb-3">*/}
       {/*    <View className="flex-row">*/}
