c1b9bc9a65bbc6853cb6cc7d34c7611ab397e412
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-09-28 13:12:53 +0300

    Building the Authentication Screen

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,11 +4,12 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
+// @ts-ignore
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-	auth: {
-		storage: AsyncStorage,
-		autoRefreshToken: true,
-		persistSession: true,
-		detectSessionInUrl: false,
-	},
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
 });
